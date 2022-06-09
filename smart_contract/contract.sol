11bc9cae37b16d1ce2b1036c7326e23af2f4bd45
nghia220800 <73370429+nghia220800@users.noreply.github.com>
2022-06-09 14:08:25 +0700

    mint - swap

--- smart_contract/contract.sol
+++ smart_contract/contract.sol
@@ -1,24 +1,19 @@
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.7;
 
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contracts/token/ERC20/IERC20.sol
 
-/*
-transfer
-approve, check allowance, transferFrom
-*/
-
-interface IERC20 {
+interface IE20 {
     function totalSupply() external view returns (uint);
 
     function balanceOf(address account) external view returns (uint);
 
-    function transfer(address recipient, uint amount) external returns (bool); // A send x to B
+    function transfer(address recipient, uint amount) external returns (bool);
 
-    function allowance(address owner, address spender) external view returns (uint); //
+    function allowance(address owner, address spender) external view returns (uint);
 
-    function approve(address spender, uint amount) external returns (bool); //A approve B to withdraw x
+    function approve(address spender, uint amount) external returns (bool);
 
-    function transferFrom(  //B withdraw x from A
+    function transferFrom(
         address sender,
         address recipient,
         uint amount
@@ -27,3 +22,26 @@ interface IERC20 {
     event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed owner, address indexed spender, uint value);
 }
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
+contract MyToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 100 * 10**uint(decimals()));         // 1 token = 10 * 18 Wei(decimal)
+    }
+}
+/*
+Wallet1
+0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+Acoin
+0xd9145CCE52D386f254917e481eB44e9943F39138
+
+Wallet2
+0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+Bcoin
+0xa131AD247055FD2e2aA8b156A11bdEc81b9eAD95
+
+TokenSwap
+0x417Bf7C9dc415FEEb693B6FE313d1186C692600F
+10000000000000000000
+20000000000000000000
+*/
--- smart_contract/tokenSwap.sol
+++ smart_contract/tokenSwap.sol
@@ -1,32 +1,73 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contracts/token/ERC20/IERC20.sol
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/IERC20.sol";
 
-interface IE20 {
-    function totalSupply() external view returns (uint);
+/*
+How to swap tokens
 
-    function balanceOf(address account) external view returns (uint);
+1. Alice has 100 tokens from AliceCoin, which is a ERC20 token.
+2. Bob has 100 tokens from BobCoin, which is also a ERC20 token.
+3. Alice and Bob wants to trade 10 AliceCoin for 20 BobCoin.
+4. Alice or Bob deploys TokenSwap
+5. Alice approves TokenSwap to withdraw 10 tokens from AliceCoin
+6. Bob approves TokenSwap to withdraw 20 tokens from BobCoin
+7. Alice or Bob calls TokenSwap.swap()
+8. Alice and Bob traded tokens successfully.
+*/
 
-    function transfer(address recipient, uint amount) external returns (bool);
+contract TokenSwap {
+    IERC20 public token1;
+    address public owner1;
+    uint public amount1;
+    IERC20 public token2;
+    address public owner2;
+    uint public amount2;
 
-    function allowance(address owner, address spender) external view returns (uint);
+    constructor(
+        address _token1,
+        address _owner1,
+        uint _amount1,
+        address _token2,
+        address _owner2,
+        uint _amount2
+    ) {
+        token1 = IERC20(_token1);
+        owner1 = _owner1;
+        amount1 = _amount1;
+        token2 = IERC20(_token2);
+        owner2 = _owner2;
+        amount2 = _amount2;
+    }
 
-    function approve(address spender, uint amount) external returns (bool);
+    event TokenSwapped(address t1, address o1, uint a1, address t2, address o2, uint a2);
 
-    function transferFrom(
+    function swap() public {
+        require(msg.sender == owner1 || msg.sender == owner2, "Not authorized");
+        require(
+            token1.allowance(owner1, address(this)) >= amount1,
+            "Token 1 allowance too low"
+        );
+        require(
+            token2.allowance(owner2, address(this)) >= amount2,
+            "Token 2 allowance too low"
+        );
+
+        _safeTransferFrom(token1, owner1, owner2, amount1);
+        _safeTransferFrom(token2, owner2, owner1, amount2);
+    }
+
+    function _safeTransferFrom(
+        IERC20 token,
         address sender,
         address recipient,
         uint amount
-    ) external returns (bool);
+    ) private {
+        bool sent = token.transferFrom(sender, recipient, amount);
+        require(sent, "Token transfer failed");
+    }
 
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
-}
-
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
-contract MyToken is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        _mint(msg.sender, 100 * 10**uint(decimals()));         // 1 token = 10 * 18 Wei(decimal)
+    function Swapped() public {
+        emit TokenSwapped(address(token1), owner1, amount1, address(token2), owner2, amount2);
     }
 }
